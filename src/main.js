28f77cc250b971ed58f3d1f687ed59717fc90b13
svvishjs <andreww92@live.com>
2021-01-07 18:55:12 +0000

    added vue-leaflet to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueSlideoutPanel from 'vue2-slideout-panel';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 Vue.use(VueSlideoutPanel);
 
